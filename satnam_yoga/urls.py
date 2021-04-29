f105c507c6b535bf7c08b40b68547a7237bfe511
oscar abraham <oscara1706cl@gmail.com>
2021-04-28 23:02:52 -0500

    modifying procfile 6

--- satnam_yoga/urls.py
+++ satnam_yoga/urls.py
@@ -29,5 +29,7 @@ urlpatterns = [
     path('', include('contact.urls')),
     path('', include('pages.urls')),
 
-] + static(settings.STATIC_URL, document_root= settings.STATIC_ROOT)
-# ] + static(settings.MEDIA_URL, document_root= settings.MEDIA_ROOT)
+]
+
+urlpatterns + static(settings.MEDIA_URL, document_root= settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root= settings.STATIC_ROOT)
