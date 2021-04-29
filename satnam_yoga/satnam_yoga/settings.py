b7925d51953ab9df0cc85e110f964de65d18240b
oscar abraham <oscara1706cl@gmail.com>
2021-04-28 22:08:37 -0500

    secret key must not be empty dice heroku

--- satnam_yoga/satnam_yoga/settings.py
+++ satnam_yoga/satnam_yoga/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY_DJANGO_SATNAM')
+SECRET_KEY = '9$xjmaxhvb8b-^i(^l#rt^y4vnphw8zk5m2d+kowly=i=gu8s@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
