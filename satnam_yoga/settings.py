d9affee0372713f7de6b66bff080bfd1a6526034
oscar abraham <oscara1706cl@gmail.com>
2021-07-20 18:36:03 -0500

    modificando la app por errores en renderear los templates

--- satnam_yoga/settings.py
+++ satnam_yoga/settings.py
@@ -21,13 +21,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", get_random_secret_key())
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', 'https://satnamyoga.herokuapp.com', 'www.satnamyogaestudio.com', 'satnamyogaestudio.com', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', 'satnamyoga.herokuapp.com', 'www.satnamyogaestudio.com', 'satnamyogaestudio.com', '127.0.0.1']
 
 
 # Application definition
