bd5415aaab3411ff715cf667e8cb557b207a618d
oscar abraham <oscara1706cl@gmail.com>
2021-07-20 21:34:58 -0500

    fixing bugs 5

--- satnam_yoga/settings.py
+++ satnam_yoga/settings.py
@@ -55,10 +55,10 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'BruteBuster.middleware.RequestMiddleware',
